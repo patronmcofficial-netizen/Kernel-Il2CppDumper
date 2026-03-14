6acd91813274899081377319f70f0bb3b1b3cbee
patronmcofficial-netizen <patronmcofficial@gmail.com>
2026-03-14 15:04:46 +0300

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef RIRU_IL2CPPDUMPER_GAME_H
 #define RIRU_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.mobile.legends"
 
 #endif //RIRU_IL2CPPDUMPER_GAME_H
